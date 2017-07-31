4c0fe89252af3f402a1ae63204c2c8b9d6b18672
harsha <alleni.harsha@gmail.com>
2017-07-31 07:23:43 +0530

    .

--- src/com/csv/DataRow.java
+++
@@ -1,7 +0,0 @@
-package com.csv;
-import java.util.*;
-
-public class DataRow extends LinkedHashMap<Integer,String>
-{
-
-}
--- src/com/csv/HeaderRow.java
+++
@@ -1,7 +0,0 @@
-package com.csv;
-import java.util.*;
-
-public class HeaderRow extends LinkedHashMap<String,Integer>
-{
-		
-}
